1d4e0e0698931bcd3c025da10d57e31345303117
McJty <mcjty1@gmail.com>
2022-11-30 06:42:12 +0100

    WIP

--- src/main/java/com/mcjty/fancytrinkets/compat/FancyJeiPlugin.java
+++ src/main/java/com/mcjty/fancytrinkets/compat/FancyJeiPlugin.java
@@ -2,8 +2,10 @@ package com.mcjty.fancytrinkets.compat;
 
 import com.mcjty.fancytrinkets.FancyTrinkets;
 import com.mcjty.fancytrinkets.modules.xpcrafter.XpCrafterModule;
+import com.mcjty.fancytrinkets.modules.xpcrafter.blocks.ExperienceCrafterBE;
 import com.mcjty.fancytrinkets.modules.xpcrafter.recipe.XpRecipe;
 import com.mcjty.fancytrinkets.setup.Registration;
+import mcjty.lib.container.GenericContainer;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.helpers.IGuiHelper;
@@ -55,6 +57,8 @@ public class FancyJeiPlugin implements IModPlugin {
 
     @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
-//        registration.addRecipeTransferHandler();
+        registration.addRecipeTransferHandler(GenericContainer.class, XpCrafterModule.CONTAINER_EXPERIENCE_CRAFTER.get(), XP_RECIPE_TYPE,
+                0, XpRecipe.RECIPE_DIMENSION * XpRecipe.RECIPE_DIMENSION,
+                ExperienceCrafterBE.SLOT_GRID + XpRecipe.RECIPE_DIMENSION * XpRecipe.RECIPE_DIMENSION, 36);
     }
 }
--- src/main/java/com/mcjty/fancytrinkets/compat/XpRecipeCategory.java
+++ src/main/java/com/mcjty/fancytrinkets/compat/XpRecipeCategory.java
@@ -18,16 +18,16 @@ import net.minecraft.world.item.ItemStack;
 
 public class XpRecipeCategory implements IRecipeCategory<XpRecipe> {
 
-    private final IGuiHelper guiHelper;
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawable slot;
 
     public static final String KEY_XP_RECIPE_CATEGORY = "fancytrinkets.xp_recipe_category";
 
     public XpRecipeCategory(IGuiHelper guiHelper) {
-        this.guiHelper = guiHelper;
+        slot = guiHelper.getSlotDrawable();
         icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(XpCrafterModule.EXPERIENCE_CRAFTER.get()));
-        background = guiHelper.createBlankDrawable(140, 110);
+        background = guiHelper.createBlankDrawable(140, 120);
     }
 
     @Override
@@ -52,6 +52,12 @@ public class XpRecipeCategory implements IRecipeCategory<XpRecipe> {
 
     @Override
     public void draw(XpRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        for (int x = 0 ; x < XpRecipe.RECIPE_DIMENSION ; x++) {
+            for (int y = 0 ; y < XpRecipe.RECIPE_DIMENSION ; y++) {
+                slot.draw(stack, 4 + x*18, 4 + y*18);
+            }
+        }
+        slot.draw(stack, 4+6*18, 4);
     }
 
     @Override
@@ -63,7 +69,7 @@ public class XpRecipeCategory implements IRecipeCategory<XpRecipe> {
 
             }
         }
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 5 + 7*18,  9)
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 5 + 6*18,  5)
                 .addItemStack(recipe.getResultItem());
 
     }
